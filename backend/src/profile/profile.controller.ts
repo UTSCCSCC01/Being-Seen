d53360a4347a69d9d77dc2587a28168b7ceca6e1
Youzhang (Mark) Sun <youzhang.mark.sun@gmail.com>
2021-10-02 18:54:17 -0400

    BRUT-32: Add parameter to getProfile() in profile.controller.ts

--- backend/src/profile/profile.controller.ts
+++ backend/src/profile/profile.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Put } from '@nestjs/common'
+import { Controller, Get, Put, Param } from '@nestjs/common'
 import { ProfileService } from './profile.service'
 
 @Controller('profiles')
@@ -6,7 +6,7 @@ export class ProfileController {
     constructor(private readonly profilesService: ProfileService) {}
 
     @Get('/:profileId')
-    async getProfile(profileId: string) {
+    async getProfile( @Param("profileId") profileId: string ) {
         const profile = await this.profilesService.getProfile(profileId);
         console.log(profile);
         return profile
