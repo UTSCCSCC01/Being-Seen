cb7e019b1838bc5caaf0608036da0d17834646be
aquila_zyy <aquila.zyy@gmail.com>
2021-10-03 18:55:51 -0400

    Added Landing page to the navigation tab

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import LandingPage from "./screens/landing_page";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
+        <Tab.Screen name="LandingPage" component={LandingPage} />
         <Tab.Screen name="Profile" component={View} />
         <Tab.Screen name="Shelters" component={View} />
         <Tab.Screen name="Social Services" component={View} />
