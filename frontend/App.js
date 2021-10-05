fb3c5ff9eab39d922f1b48252e0df9b514e6a098
aquila_zyy <aquila.zyy@gmail.com>
2021-10-04 21:00:15 -0400

    BRUT-30: Replaced scrollview with flatlist header component thus resolving the nested virtualist issue.

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Shelter from './screens/Shelter';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Shelter from "./screens/Shelter";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
         <Tab.Screen name="Profile" component={View} />
         <Tab.Screen name="Social Services" component={Shelter} />
       </Tab.Navigator>
