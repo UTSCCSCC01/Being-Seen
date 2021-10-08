c5e3a091dc3c6f3e4ac3e5e6a8494a49bee9d886
Glenn Ye <glenn.ye38@gmail.com>
2021-10-08 03:24:04 -0400

    Merge branch 'feature-BRUT-27' into develop

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Shelter from "./screens/Shelter";
 import Login from './screens/Login';
 
 const Tab = createBottomTabNavigator();
@@ -14,13 +15,12 @@ const Tab = createBottomTabNavigator();
  */
 export default function App() {
   return (
-    // <NavigationContainer>
-    //   <Tab.Navigator>
-    //     <Tab.Screen name="Profile" component={View} />
-    //     <Tab.Screen name="Shelters" component={View} />
-    //     <Tab.Screen name="Social Services" component={View} />
-    //   </Tab.Navigator>
-    // </NavigationContainer>
-  <Login />
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Profile" component={View} />
+        <Tab.Screen name="Social Services" component={Shelter} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  // <Login />
   );
 }
