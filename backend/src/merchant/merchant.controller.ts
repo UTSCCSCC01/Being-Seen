bacd3087f65e857f1ec180831e1e11fdc36a56a8
aquila_zyy <aquila.zyy@gmail.com>
2021-10-19 13:44:26 -0400

    BRUT-64: Implemented deleteMerchant. Added JSdoc.

--- backend/src/merchant/merchant.controller.ts
+++ backend/src/merchant/merchant.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
-import * as mongoose from 'mongoose';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Tag } from 'src/Schemas/tag.schema';
 import { MerchantService } from './merchant.service';
 import { Merchant } from 'src/Schemas/merchant.schema';
@@ -11,18 +19,72 @@ import { Merchant } from 'src/Schemas/merchant.schema';
 export class MerchantController {
   constructor(private readonly merchantService: MerchantService) {}
 
+  /**
+   * NestJS handler. The request uri to this endpoint is:
+   *
+   *   GET .../merchant/
+   *
+   * with no body payload.
+   *
+   * Gets all merchants.
+   *
+   * @returns A list of all merchants.
+   */
   @Get()
   async getMerchantsHandler(): Promise<Merchant[]> {
     let merchants = await this.merchantService.getAllMerchants();
     return merchants;
   }
 
+  /**
+   * NestJS handler. The request uri to this endpoint is:
+   *
+   *   GET .../merchant/<:id>
+   *
+   * with no body payload.
+   *
+   * Gets the merchant with the object id <:id>.
+   *
+   * @param id The id of the merchant of interest.
+   * @returns The requested merchant.
+   */
   @Get(':id')
   async getMerchantByIdHandler(@Param('id') id: string): Promise<Merchant> {
     let merchant = await this.merchantService.getMerchantById(id);
     return merchant;
   }
 
+  /**
+   * NestJS handler. The request uri to this endpoint is:
+   *
+   *   POST .../merchant/
+   *
+   * with a body containing a json file of the format:
+   * {
+   *   "name": "Test Merchant",
+   *   "description": "This is a description",
+   *   "address": "1234 abcd Rd.",
+   *   "hours": "9-5",
+   *   "tags": [
+   *     {
+   *       "id": "615b140f04816ef571baa486"
+   *     },
+   *     {
+   *       "id": "615b142f04816ef571baa48f"
+   *     }
+   *   ]
+   * }
+   *
+   * Creates a new merchant with the provided fields.
+   *
+   * @param name The name of the merchant to be created.
+   * @param description The description of the merchant to be created.
+   * @param tags The tags of the merchant to be created.
+   * @param address The address of the merchant to be created.
+   * @param hours The working hours of the merchant to be created.
+   * @param picture The thumbnail of the merchant to be created.
+   * @returns The object id of the merchant created.
+   */
   @Post()
   async createMerchantHandler(
     @Body('name') name: string,
@@ -32,13 +94,34 @@ export class MerchantController {
     @Body('hours') hours: string,
     @Body('picture') picture: string,
   ): Promise<string> {
-    let merchantId = await this.merchantService.createMerchant(
+    let merchantId;
+    merchantId = await this.merchantService.createMerchant(
       name,
       description,
       address,
       hours,
       picture,
+      tags,
     );
     return merchantId;
+  }
+
+  /**
+   * NestJS handler. The request uri to this endpoint is:
+   *
+   *   DELETE .../merchant/<:id>
+   *
+   * with no body payload.
+   *
+   * Deletes the merchant with object id <:id>
+   *
+   * @param id The object id of the merchant to be deleted.
+   * @returns Number of merchants deleted. Should be 1 on success,
+   * and 0 on failure.
+   */
+  @Delete(':id')
+  async deleteMerchantHandler(@Body('id') id: string): Promise<Number> {
+    let deleteCount = await this.merchantService.deleteMerchant(id);
+    return deleteCount;
   }
 }
--- backend/src/merchant/merchant.service.ts
+++ backend/src/merchant/merchant.service.ts
@@ -4,6 +4,7 @@ import { Model, Mongoose, ObjectId, Schema, Types } from 'mongoose';
 import { execPath } from 'process';
 import { MerchantDocument } from 'src/Schemas/merchant.schema';
 import { Merchant } from 'src/Schemas/merchant.schema';
+import { Tag } from 'src/Schemas/tag.schema';
 
 @Injectable()
 export class MerchantService {
@@ -14,6 +15,7 @@ export class MerchantService {
 
   /**
    * Returns an array of Merchant instances.
+   *
    * @returns All merchants in the database.
    */
   async getAllMerchants(): Promise<Merchant[]> {
@@ -21,6 +23,11 @@ export class MerchantService {
     return merchants as Merchant[];
   }
 
+  /**
+   * Returns a merchant that has the object id of <id>.
+   * @param id The object id of the merchant to be returned.
+   * @returns The requested merchant.
+   */
   async getMerchantById(id: string): Promise<Merchant> {
     let merchant: Merchant;
     try {
@@ -34,12 +41,24 @@ export class MerchantService {
     return merchant;
   }
 
+  /**
+   * Create a new merchant.
+   *
+   * @param name The name of the merchant to be created.
+   * @param description The description of the merchant to be created.
+   * @param tags The tags of the merchant to be created.
+   * @param address The address of the merchant to be created.
+   * @param hours The working hours of the merchant to be created.
+   * @param picture The thumbnail of the merchant to be created.
+   * @returns The object id of the merchant created.
+   */
   async createMerchant(
     name: string,
     description: string,
     address: string,
     hours: string,
     picture: string,
+    tags: Tag[],
   ): Promise<string> {
     const newMerchant = new this.merchantModel({
       name: name,
@@ -47,8 +66,40 @@ export class MerchantService {
       address: address,
       hours: hours,
       pircture: picture,
+      tags: this.convertOidArr(tags),
     });
     newMerchant.save();
     return newMerchant.id;
+  }
+
+  /**
+   * Delete the merchant with the object id <id>.
+   *
+   * @param id The object id of the merchant to be deleted.
+   * @returns Number of objects affected. Should be 1 on success,
+   * and 0 on faliure.
+   */
+  async deleteMerchant(id: string) {
+    let deleteCount: Number;
+    try {
+      deleteCount = (await this.merchantModel.deleteOne({ id: id })).n;
+    } catch (error) {
+      throw new NotFoundException('No such merchant to delete!');
+    }
+    if (deleteCount === 0) {
+      throw new NotFoundException('No such merchant to delete!');
+    }
+    return deleteCount;
+  }
+
+  private convertOidArr(arr) {
+    try {
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].id = Types.ObjectId(arr[i].id);
+      }
+      return arr;
+    } catch (error) {
+      return [];
+    }
   }
 }
