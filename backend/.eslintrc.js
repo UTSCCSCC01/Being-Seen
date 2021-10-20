3fd1e2a34f94fd82a2b43e094c864e011e03850a
Glenn Ye <glenn.ye38@gmail.com>
2021-10-20 00:29:15 -0400

    [BRUT-74] Remove import and CRLF eslint warnings

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'backend/tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -20,5 +20,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ["error", {
+      "endOfLine": "auto",
+    }],
   },
 };
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,7 +2,6 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './users.schema';
-import * as crypto from 'crypto';
 
 @Injectable()
 export class UsersService {
