0f10138059de18d69a412c92b2b8b79ddc09cc10
Glenn Ye <glenn.ye38@gmail.com>
2021-10-22 03:02:45 -0400

    [BRUT-81] Create forgot password screen

--- frontend/components/UnderlinedLink.jsx
+++ frontend/components/UnderlinedLink.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, TouchableHighlight } from 'react-native';
 import { tailwind } from 'tailwind';
+import { useNavigation } from '@react-navigation/native';
 
 /**
  * @function UnderlinedLink
  * @module UnderlinedLink
  * @description An underlined text link
  * @prop {string} text - The text to display
+ * @prop {string} to
  */
-const UnderlinedLink = ({ text }) => (
-  <Text style={styles.text}>{text}</Text>
-);
+const UnderlinedLink = ({ text, to }) => {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableHighlight onPress={() => navigation.push(to)} underlayColor="gray">
+      <Text style={styles.text}>{text}</Text>
+    </TouchableHighlight>
+  );
+};
 
 UnderlinedLink.propTypes = {
   text: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
 };
 
 const styles = StyleSheet.create({
---
+++ frontend/screens/RecoverAccountScreen.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, View, Linking, TouchableHighlight } from 'react-native';
+import { tailwind } from 'tailwind';
+import ScreenHeader from '../components/ScreenHeader';
+import Button from '../components/Button';
+import { useNavigation } from '@react-navigation/native';
+
+const RecoverAccountScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView>
+      <ScreenHeader
+        leftNode={
+          <View>
+            <TouchableHighlight onPress={() => navigation.goBack()} underlayColor="transparent">
+              <Text style={styles.backButton}>Back</Text>
+            </TouchableHighlight>
+          </View>
+        }
+        headerText="Recover Account"
+      />
+      <View style={styles.body}>
+        <Text style={styles.text}>Can't log in? Don't worry! Email us and we will help you go through a series of steps to 
+        securely recover your account.{'\n'}{'\n'}
+        Click below to email us why you can't log in. We will be contacting you shortly with next steps.</Text>
+      <View style={styles.buttonView}>
+        <Button 
+          label="Recover account"
+          onClick={() => Linking.openURL('mailto:support@beingseen.com?subject=Can\'t Log In&body=') }
+          disabled={false}
+        />
+      </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  backButton: {
+    ...tailwind('text-primaryPurple text-opacity-90 text-xl'),
+  },
+  body: {
+    ...tailwind('px-4 py-6'),
+  },
+  text: {
+    ...tailwind('text-primaryPurple text-base'),
+  },
+  buttonView: {
+    ...tailwind('px-4 py-8'),
+  },
+});
+
+export default RecoverAccountScreen;
