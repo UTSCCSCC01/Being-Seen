46507f2c96098c7bb2dfde02620e3456e8d515d4
aquila_zyy <aquila.zyy@gmail.com>
2021-10-30 06:06:46 -0400

    BRUT-115: Added jsdoc for APIHandler and FormatHelper

--- frontend/util/APIHandler.js
+++ frontend/util/APIHandler.js
@@ -14,7 +14,7 @@ async function __sendReviewToAPI(
   rating,
   method
 ) {
-  if (content === undefined || isNaN(rating)) {
+  if (content === undefined || Number.isNaN(rating)) {
     throw new Error("Content is undefined or rating is NaN");
   }
   const body = JSON.stringify({ content, rating });
@@ -26,18 +26,35 @@ async function __sendReviewToAPI(
 }
 
 export default {
+  /**
+   * Sends a GET request to the endpoint at serviceType.
+   * @param {string} serviceType The name of the endpoint to which this http request will be sent.
+   * @returns A Promise of response from the server.
+   */
   getInfoFromApi(serviceType) {
     return fetch(apiAddress + serviceType, {
       method: "Get",
     });
   },
 
+  /**
+   * Sends a GET request to the endpoint at serviceType, with id concatenated.
+   * @param {string} serviceType The name of the endpoint to which this http request will be sent.
+   * @param {string} id The objectId of a database record.
+   * @returns The Promise of a record in the database with objectId as its id.
+   */
   getInfoFromApiById(serviceType, id) {
     return fetch(`${apiAddress + serviceType}/${id}`, {
       method: "Get",
     });
   },
 
+  /**
+   * Sends the login credentials to the server.
+   * @param {string} username The username of the login credential.
+   * @param {string} password The password of the login credential.
+   * @returns A Promise of a response from the server, possibly containing an access token.
+   */
   submitLogin(username, password) {
     return fetch(`${apiAddress}auth/login`, {
       method: "POST",
@@ -51,6 +68,12 @@ export default {
     });
   },
 
+  /**
+   * Sends a list of strings as tags to the server.
+   * @param {string[]} tagList An array of strings. Each one represents a tag to be searched.
+   * @param {string} serviceType The name of the endpoint to which this http request will be sent.
+   * @returns A Promise of an array of database records.
+   */
   getSearchResult(tagList, serviceType) {
     const payload = JSON.stringify({
       tagList,
@@ -62,6 +85,15 @@ export default {
     });
   },
 
+  /**
+   * Gets this reviewer's review on this service.
+   * @param {string} serviceId The objectId of the service of interest.
+   * @param {string} reviewerId The objectId of the reviewer's profile.
+   * @param {string} serviceType The name of the endpoint to which this http request will be sent.
+   * @returns A Promise of the reviewer's review on this service. If this
+   *          reviewer has not reviewed this service, the status code would
+   *          be 404.
+   */
   getReviewFromApi(serviceId, reviewerId, serviceType) {
     return fetch(
       `${apiAddress + serviceType}/${serviceId}/review/${reviewerId}`,
@@ -71,6 +103,13 @@ export default {
     );
   },
 
+  /**
+   * Deletes this reviewer's review on this service.
+   * @param {string} serviceId The objectId of the service of interest.
+   * @param {string} reviewerId reviewerId The objectId of the reviewer's profile.
+   * @param {string} serviceType The name of the endpoint to which this http request will be sent.
+   * @returns A Promise of a response from the server. 200 on success.
+   */
   deleteReviewFromApi(serviceId, reviewerId, serviceType) {
     return fetch(
       `${apiAddress + serviceType}/${serviceId}/review/${reviewerId}`,
@@ -80,12 +119,24 @@ export default {
     );
   },
 
+  /**
+   * Gets the jwt token.
+   * @returns The jwt token.
+   */
   async getProfileIdFromToken() {
     const token = await SecureStore.getItemAsync("token");
     const decoded = await jwt_decode(token);
     return decoded.id;
   },
 
+  /**
+   * Sends a new review to the server and creates it in the database.
+   * @param {string} serviceId The objectId of the service of interest.
+   * @param {string} reviewerId The objectId of the reviewer's profile.
+   * @param {string} serviceType The name of the endpoint to which this http request will be sent.
+   * @param {string} content The content of this review.
+   * @param {Number} rating The rating of thie review.
+   */
   async postReviewToApi(serviceId, reviewerId, serviceType, content, rating) {
     __sendReviewToAPI(
       serviceId,
@@ -97,6 +148,14 @@ export default {
     );
   },
 
+  /**
+   * Sends an edited review to the server and overwrites it in the database.
+   * @param {string} serviceId The objectId of the service of interest.
+   * @param {string} reviewerId The objectId of the reviewer's profile.
+   * @param {string} serviceType The name of the endpoint to which this http request will be sent.
+   * @param {string} content The content of this review.
+   * @param {Number} rating The rating of thie review.
+   */
   async patchReviewToApi(serviceId, reviewerId, serviceType, content, rating) {
     __sendReviewToAPI(
       serviceId,
