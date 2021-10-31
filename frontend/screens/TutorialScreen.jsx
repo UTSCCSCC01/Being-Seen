ab61ed0819609858c8ab81e9f4b10899808152db
Glenn Ye <glenn.ye38@gmail.com>
2021-10-31 02:57:57 -0400

    [BRUT-99] Fix positioning of slider bar

--- frontend/screens/TutorialScreen.jsx
+++ frontend/screens/TutorialScreen.jsx
@@ -50,109 +50,110 @@ const TutorialScreen = () => {
   };
 
   return (
-    <View>
-      <SafeAreaView style={styles({}).tutorialView}>
-        <ScrollView
-          style={styles({}).slideshowView}
-          horizontal
-          scrollEventThrottle={16}
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          onScroll={(event) => {
-            setSliderPage(event);
-          }}
-        >
-          <View style={styles({ width, height }).slide}>
-            <Image source={images.landing_page_bg_1} style={styles({}).image} />
-            <View style={styles({}).textBox}>
-              <PrimaryHeader text="Welcome" />
-              <Text style={styles({}).description}>
-                Being Seen aims to help you easily access resources relevant to
-                the homeless community.
-              </Text>
-            </View>
-          </View>
-          <View style={styles({ width, height }).slide}>
-            <Image source={images.landing_page_bg_1} style={styles({}).image} />
-            <View style={styles({}).textBox}>
-              <PrimaryHeader text="Newsreel" />
-              <Text style={styles({}).description}>
-                Check out recently posted services and events.
-              </Text>
-            </View>
-          </View>
-          <View style={styles({ width, height }).slide}>
-            <Image source={images.landing_page_bg_1} style={styles({}).image} />
-            <View style={styles({}).textBox}>
-              <PrimaryHeader text="Profile" />
-              <Text style={styles({}).description}>
-                View your profile and edit your information.
-              </Text>
-            </View>
-          </View>
-          <View style={styles({ width, height }).slide}>
-            <Image source={images.landing_page_bg_1} style={styles({}).image} />
-            <View style={styles({}).textBox}>
-              <PrimaryHeader text="Search" />
-              <Text style={styles({}).description}>
-                Search for services by tag name.
-              </Text>
-            </View>
-          </View>
-          <View style={styles({ width, height }).slide}>
-            <Image source={images.landing_page_bg_1} style={styles({}).image} />
-            <View style={styles({}).textBox}>
-              <PrimaryHeader text="Merchants" />
-              <Text style={styles({}).description}>
-                Check out partnered stores for discounts and coupons.
-              </Text>
-            </View>
-          </View>
-          <View style={styles({ width, height }).slide}>
-            <Image source={images.landing_page_bg_1} style={styles({}).image} />
-            <View style={styles({}).textBox}>
-              <PrimaryHeader text="Jobs" />
-              <Text style={styles({}).description}>
-                Browse through job postings and easily apply for jobs you are
-                interested in.
-              </Text>
-            </View>
-          </View>
-          <View style={styles({ width, height }).slide}>
-            <Image source={images.landing_page_bg_1} style={styles({}).image} />
-            <View style={styles({}).textBox}>
-              <PrimaryHeader text="Social Services" />
-              <Text style={styles({}).description}>
-                Get a list of relevant social services, such as shelters, food
-                banks, and safe injection sites. You can also read and leave
-                user reviews.
-              </Text>
-            </View>
-          </View>
-          <View style={styles({ width, height }).slide}>
-            <Image source={images.landing_page_bg_1} style={styles({}).image} />
-            <View style={styles({}).textBox}>
-              <PrimaryHeader text="Education" />
-              <Text style={styles({}).description}>
-                View upcoming education and mentorship opportunities.
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
-        <View style={styles({}).sliderView}>
-          {Array.from(Array(8).keys()).map((key, index) => (
-            <View style={styles({ pageIndex, index }).sliderDot} key={key} />
-          ))}
-          <View style={styles({}).exit}>
-            {checkIfFirstLaunch() ? (
-              <UnderlinedLink text="Exit Tutorial" to="Login" />
-            ) : (
-              <UnderlinedLink text="Exit Tutorial" back />
-            )}
+    <SafeAreaView style={styles({}).tutorialView}>
+      <ScrollView
+        style={styles({}).slideshowView}
+        horizontal
+        scrollEventThrottle={16}
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={(event) => {
+          setSliderPage(event);
+        }}
+      >
+        <View style={styles({ width, height }).slide}>
+          <Image source={images.landing_page_bg_1} style={styles({}).image} />
+          <View style={styles({}).textBox}>
+            <PrimaryHeader text="Welcome" />
+            <Text style={styles({}).description}>
+              Being Seen aims to help you easily access resources relevant to
+              the homeless community.
+            </Text>
           </View>
         </View>
-      </SafeAreaView>
-    </View>
+        <View style={styles({ width, height }).slide}>
+          <Image source={images.landing_page_bg_1} style={styles({}).image} />
+          <View style={styles({}).textBox}>
+            <PrimaryHeader text="Newsreel" />
+            <Text style={styles({}).description}>
+              Check out recently posted services and events.
+            </Text>
+          </View>
+        </View>
+        <View style={styles({ width, height }).slide}>
+          <Image source={images.landing_page_bg_1} style={styles({}).image} />
+          <View style={styles({}).textBox}>
+            <PrimaryHeader text="Profile" />
+            <Text style={styles({}).description}>
+              View your profile and edit your information.
+            </Text>
+          </View>
+        </View>
+        <View style={styles({ width, height }).slide}>
+          <Image source={images.landing_page_bg_1} style={styles({}).image} />
+          <View style={styles({}).textBox}>
+            <PrimaryHeader text="Search" />
+            <Text style={styles({}).description}>
+              Search for services by tag name.
+            </Text>
+          </View>
+        </View>
+        <View style={styles({ width, height }).slide}>
+          <Image source={images.landing_page_bg_1} style={styles({}).image} />
+          <View style={styles({}).textBox}>
+            <PrimaryHeader text="Merchants" />
+            <Text style={styles({}).description}>
+              Check out partnered stores for discounts and coupons.
+            </Text>
+          </View>
+        </View>
+        <View style={styles({ width, height }).slide}>
+          <Image source={images.landing_page_bg_1} style={styles({}).image} />
+          <View style={styles({}).textBox}>
+            <PrimaryHeader text="Jobs" />
+            <Text style={styles({}).description}>
+              Browse through job postings and easily apply for jobs you are
+              interested in.
+            </Text>
+          </View>
+        </View>
+        <View style={styles({ width, height }).slide}>
+          <Image
+            source={images.landing_page_bg_1}
+            style={styles({ height }).image}
+          />
+          <View style={styles({}).textBox}>
+            <PrimaryHeader text="Social Services" />
+            <Text style={styles({}).description}>
+              Get a list of relevant social services, such as shelters, food
+              banks, and safe injection sites. You can also read and leave user
+              reviews.
+            </Text>
+          </View>
+        </View>
+        <View style={styles({ width, height }).slide}>
+          <Image source={images.landing_page_bg_1} style={styles({}).image} />
+          <View style={styles({}).textBox}>
+            <PrimaryHeader text="Education" />
+            <Text style={styles({}).description}>
+              View upcoming education and mentorship opportunities.
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles({}).sliderView}>
+        {Array.from(Array(8).keys()).map((key, index) => (
+          <View style={styles({ pageIndex, index }).sliderDot} key={key} />
+        ))}
+        <View style={styles({}).exit}>
+          {checkIfFirstLaunch() ? (
+            <UnderlinedLink text="Exit Tutorial" to="Login" />
+          ) : (
+            <UnderlinedLink text="Exit Tutorial" back />
+          )}
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -178,7 +179,7 @@ const styles = ({ width, height, pageIndex, index }) =>
       opacity: pageIndex === index ? 1 : 0.2,
     },
     sliderView: {
-      ...tailwind("absolute flex-row inset-x-0 justify-center items-center"),
+      ...tailwind("flex-row items-center justify-center"),
       bottom: 75,
     },
     slideshowView: {
@@ -188,7 +189,7 @@ const styles = ({ width, height, pageIndex, index }) =>
       ...tailwind("justify-center items-center my-8"),
     },
     tutorialView: {
-      ...tailwind(""),
+      ...tailwind("flex-col"),
     },
   });
 
